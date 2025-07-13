78f414ccfbc1b991db2bfccc827379d11286473d
superkookai <weerawut@live.com>
2025-07-13 12:29:12 +0700

    Initial Commit

---
+++ GreenThumb/GreenThumbApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenThumbApp.swift
+//  GreenThumb
+//
+//  Created by Weerawut Chaiyasomboon on 13/07/2568.
+//
+
+import SwiftUI
+
+@main
+struct GreenThumbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
